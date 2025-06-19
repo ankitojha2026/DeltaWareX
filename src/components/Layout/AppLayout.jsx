2ea5e62eb68a5aaefd9676450910a16d70250c21
ankitojha2026 <ojhaankit064@gmail.com>
2025-06-19 11:57:07 +0530

    new update

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../UI/NavBar";
-import NavBar2 from "../UI/NavBar2";
+// import NavBar2 from "../UI/NavBar2";
 import Footer from "../UI/Footer";
 
 const AppLayout = () => {
     return <>
-    <NavBar2/>
+    {/* <NavBar2/> */}
    <NavBar/>
   
 
--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -1,18 +1,19 @@
+import { FiAlignJustify } from "react-icons/fi";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
    <>
-    <nav className="navbar navbar-expand-lg navbar-light px-5 md-px-3" style={{ backgroundColor: "#ffffff" }}>
+    <nav className="navbar navbar-expand-lg  px-2 md-px-3 shadow-lg" style={{ backgroundColor: "#212529"  }}>
       <div className="container-fluid">
         {/* Logo/Brand - Left Side */}
-        <a className="navbar-brand" href="/">
-          <h1 style={{ margin: 0 }}>DeltaWareX</h1> {/* Added margin:0 to h1 for better alignment */}
-        </a>
+        <NavLink className="navbar-brand" to="/">
+          <h1 style={{ margin: 0 ,color: "#fff" }}>DeltaWareX</h1> {/* Added margin:0 to h1 for better alignment */}
+        </NavLink>
         
         {/* Mobile Toggle Button */}
         <button 
-          className="navbar-toggler" 
+          className="navbar-toggler text-light" 
           type="button" 
           data-bs-toggle="collapse" 
           data-bs-target="#mainNavbar" 
@@ -20,7 +21,7 @@ const NavBar = () => {
           aria-expanded="false" 
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className=" text-light "><FiAlignJustify size={30} /></span>
         </button>
 
         {/* Nav Links - Right Side */}
@@ -28,29 +29,29 @@ const NavBar = () => {
           <ul className="navbar-nav">
 
             <li className="nav-item">
-              <NavLink  to="/"  className="nav-link active fw-bold fs-6 hover-effect" >
+              <NavLink  to="/"  className="nav-link text-light active fw-bold fs-6 hover-effect" >
               Home
               </NavLink>
             </li>
 
             <li className="nav-item">
-              <NavLink to="/about" className="nav-link fw-bold fs-6 hover-effect">About</NavLink>
+              <NavLink to="/about" className="nav-link text-light fw-bold fs-6 hover-effect">About</NavLink>
             </li>
 
 
             <li className="nav-item">
-              <NavLink className="nav-link fw-bold fs-6 hover-effect" to="/career">Career</NavLink> {/* Fixed spelling from "Carrier" to "Career" */}
+              <NavLink className="nav-link fw-bold fs-6 text-light hover-effect" to="/career">Career</NavLink> {/* Fixed spelling from "Carrier" to "Career" */}
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link fw-bold fs-6 hover-effect" to="/services">Services</NavLink>
+              <NavLink className="nav-link fw-bold fs-6 text-light hover-effect" to="/services">Services</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link fw-bold fs-6 hover-effect" to="/contact">Contact</NavLink>
+              <NavLink className="nav-link fw-bold fs-6 text-light hover-effect" to="/contact">Contact</NavLink>
             </li>
           </ul>
         </div>
       </div>
-    </nav>
+    </nav> 
     </>
   );
 };
